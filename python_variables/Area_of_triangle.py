544304f77002b51fa6b6e50168471147999f4063
Muneeb Rasheed <59506227+99muneeb@users.noreply.github.com>
2022-02-15 20:44:10 +0500

    Add files via upload

---
+++ python_variables/Area_of_triangle.py
@@ -0,0 +1,7 @@
+# Write a function that takes ,from the user , the length of the base and the geight of a triangle .
+# Make the function output the resulting area of the triangle as an integer  
+def traingle(base,height):
+    return int(base*height*0.5)
+# area=traingle(3,4)
+area=traingle(7,14)
+print(area)
